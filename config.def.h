03670602e047f58cf95469f9941f1e8a3f8cf5f7
ysr <141933879+jysr0@users.noreply.github.com>
2026-02-17 14:50:33 +0100

    Add files via upload

--- config.def.h
+++ config.def.h
@@ -276,23 +276,25 @@ static uint forcemousemod = ShiftMask;
  */
 const unsigned int mousescrollincrement = 3;
 static MouseShortcut mshortcuts[] = {
-	/* mask                   button              function        argument                         release */
+	/* mask                                  button              function        argument                         release */
 	
-	{ ShiftMask,              Button4,            ttysend,        {.s = "\033[5;2~"}                 },
-	{ XK_ANY_MOD,             Button4,            ttysend,        {.s = "\031"}                      },
-	{ ShiftMask,              Button5,            ttysend,        {.s = "\033[6;2~"}                 },
-	{ XK_ANY_MOD,             Button5,            ttysend,        {.s = "\005"}                      },
+	{ ShiftMask,                             Button4,            ttysend,        {.s = "\033[5;2~"}                 },
+	//{ XK_ANY_MOD,                            Button4,            ttysend,        {.s = "\031"}                      },
+	{ ControlMask|ShiftMask,                 Button4,            ttysend,        {.s = "\031"}                      },
+	{ ShiftMask,                             Button5,            ttysend,        {.s = "\033[6;2~"}                 },
+	//{ XK_ANY_MOD,                            Button5,            ttysend,        {.s = "\005"}                      },
+	{ ControlMask|ShiftMask,                 Button5,            ttysend,        {.s = "\005"}                      },
 	
 	// paste selection
-	{ XK_ANY_MOD,             Button2,            selpaste,       {.i = 0},                        1 },
+	{ XK_ANY_MOD,                            Button2,            selpaste,       {.i = 0},                        1 },
 	
 	// scrollback
-	//{ ShiftMask,            Button4,            kscrollup,      {.i = 1}                           },
-	//{ ShiftMask,            Button5,            kscrolldown,    {.i = 1}                           },
-	//{ XK_ANY_MOD,           Button4,            kscrollup,      {.i = 1},		0, /* !alt */ -1 },
-	//{ XK_ANY_MOD,           Button5,            kscrolldown,    {.i = 1},		0, /* !alt */ -1 },
-	{ XK_ANY_MOD,             Button4,            kscrollup,      {.i = mousescrollincrement}        },
-	{ XK_ANY_MOD,             Button5,            kscrolldown,    {.i = mousescrollincrement}        },
+	//{ ShiftMask,                           Button4,            kscrollup,      {.i = 1}                           },
+	//{ ShiftMask,                           Button5,            kscrolldown,    {.i = 1}                           },
+	//{ XK_ANY_MOD,                          Button4,            kscrollup,      {.i = 1},		0, /* !alt */ -1 },
+	//{ XK_ANY_MOD,                          Button5,            kscrolldown,    {.i = 1},		0, /* !alt */ -1 },
+	{ XK_ANY_MOD,                            Button4,            kscrollup,      {.i = mousescrollincrement}        },
+	{ XK_ANY_MOD,                            Button5,            kscrolldown,    {.i = mousescrollincrement}        },
 
 	// clickurl
 	// hold ControlMask then mouse button 1 to click on a URL.
