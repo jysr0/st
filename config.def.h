89aa64aa55560f394bfd92d69ba62d454e0d44e9
ysr <141933879+jysr0@users.noreply.github.com>
2026-03-20 23:54:44 +0100

    Add files via upload

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "DejaVuSansM Nerd Font Mono:style=Regular:pixelsize=15:antialias=true:autohint=true";
+static char *font = "DejaVuSansM Nerd Font Mono:style=Regular:pixelsize=14:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
     "DejaVuSansM Nerd Font Mono"
