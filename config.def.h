66514a9d0b0b0cc930181a6a67e0f958312905a6
ysr <141933879+jysr0@users.noreply.github.com>
2026-03-22 23:58:22 +0100

    Just font change.

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "CaskaydiaMono NFM:style=SemiLight:pixelsize=12.3:antialias=true:autohint=true";
+static char *font = "CaskaydiaMono NFM:style=Regular:pixelsize=13:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
     "CaskaydiaMono NFM"
